a1198bd99c5248550b0f3373b7501ef902ca3b30
Otto <ohotto@gmail.com>
2015-02-13 01:51:44 +0800

    Try using dropbox instead of Cloudinary

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,6 @@ CarrierWave.configure do |config|
   config.dropbox_access_token = "6yio3k16cldj2fbt"
   config.dropbox_access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
   config.dropbox_user_id = ENV["USER_ID"]
-  config.dropbox_access_type = "dropbox"
+  config.dropbox_access_type = "app_folder"
 end
 
