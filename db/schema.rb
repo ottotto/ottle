4e37dd1beabcf17470e5b64ae485a4484dd25705
Otto <ohotto@gmail.com>
2015-02-15 22:07:15 +0800

    Adding Google Maps

--- db/schema.rb
+++ db/schema.rb
@@ -26,16 +26,6 @@ ActiveRecord::Schema.define(version: 20150215091153) do
     t.integer  "user_id"
   end
 
-  create_table "locations", force: true do |t|
-    t.string   "name"
-    t.string   "address"
-    t.float    "longitude"
-    t.float    "latitude"
-    t.boolean  "gmaps"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "models", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
