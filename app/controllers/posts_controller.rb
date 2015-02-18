66e6f9df6e5782cba9f34ab6eeb2bbab3fd489cc
Otto <ohotto@gmail.com>
2015-02-18 16:13:56 +0800

    Add Address

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    if @posts.nil?
+    if params[:posts]
       @posts = Post.order("FromDate DESC").all
     else
       @posts. Post.all
